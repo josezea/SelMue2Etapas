8e86c582b96ee7e1fc54a2fd4163a82ae91b0ec7
josezea <jfzeac@unal.edu.co>
2017-05-22 21:14:35 -0500

    Add files via upload

--- SeleccionBietápico.R
+++ SeleccionBietápico.R
@@ -3,6 +3,7 @@
 
 library(TeachingSampling)
 data(BigLucy)
+library(dplyr)
 
 # UPM: Zone
 # USM: Empresas
@@ -21,11 +22,20 @@ mueUPM <- getdata(BigLucy, indica_UPM)
 #mueUPM <- BigLucy[indica_UPM$ID_unit,]
 
 # Seleccionar las USM (10 % empresas)
-N_i <- table(as.character(mueUPM$Zone))
+N_i <- table(as.character(mueUPM$Zone)); length(N_i)
 n_i <- round(0.1 * N_i)
 # Ordenar la base por Zone
 mueUPM <- mueUPM[order(mueUPM$Zone),]
+#mueUPM <- arrange(mueUPM, Zone) # library(dplyr)
 set.seed(220517)
 indicaselUSM <- strata(data = mueUPM, stratanames = "Zone",
        size = n_i, method = "srswor", description = T)
 muestra <- getdata(mueUPM, indicaselUSM )
+tamanos <- as.data.frame(N_i) 
+names(tamanos) <- c("Zone", "N_i")
+muestra <- merge(muestra, tamanos, by = "Zone")
+
+# Proceso de estimación
+# paso 1: calcular los t_i gorro
+muestra %>% group_by(Zone) %>% summarise(N_i = ,
+                                         n_i = n(), SumaY_i = sum(Income))
